62ee04b1cf66ea8f51731c2a1e0b342bd73882bd
Rishabh Chaurasia <102139510+Rishabh211103@users.noreply.github.com>
2024-01-08 20:05:28 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+package datastructures.hashtable;
+
+public class main {
+    public static void main(String[]args){
+        hashtable myhashtable = new hashtable();
+
+        myhashtable.printTable();
+    }
+}
